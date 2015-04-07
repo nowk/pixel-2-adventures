333985c06ff9b78f115b46de12062b9fc80a0a3e
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-04-07 13:13:38 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,44 @@
+# Pixel 2 Adventures
+
+Some basics to setting up a Google Chromebook Pixel 2 (2015) into a development environment.
+
+*This is particular to my setup and not a general guide for installation.*
+
+__Use at your own risk__
+
+
+## Few things to note
+
+* Don't mess with python
+
+  It comes preinstalled so no need to do anything. Breaking python links will break installations later down the line. Particularly regarding vim + python, ex: YouCompleteMe.
+
+
+* Switch terminal to `UTF-8`
+
+  If using stock fxce, it will be set to something else.
+
+
+* Default crouton browser *sucks* (no other way to put it)
+
+  Download Chrome Linux in ChromeOS mode and just install via shared download folder.
+
+
+* Fonts are not bad, but it's not great.
+
+  Installing a very large font-package helps. But you probably can get away with a lot (a lot) less.
+
+
+* Switching back and forth causes the mouse to freak on when returning back into crouton.
+
+  This only happens when moving into linux, going back to ChromeOS is fine.
+
+
+* Trackpad is a little jittery
+
+  A lot actually. Can be mittigated, but it is not entirely cured.
+
+
+* I've probably forgotten something
+
+  This was all written after, so I might be missing something.
---
+++ install.sh
@@ -0,0 +1,87 @@
+# > USE AT YOUR OWN RISK
+# > YOU BREAK IT YOU BUY IT
+
+# Install crouton
+#
+# Follow general steps in any walkthrough, then proceed
+
+# WARNING do not mess with python, unless you know what you are doing. Many, many
+# things will come to depend on it and breaking any linkage will cause agita.
+
+# apt-get
+#
+sudo apt-get update
+sudo apt-get upgrade
+
+
+# fonts, this is large, but fixes some fonting issues I noticed within chrome 
+# browser
+#
+sudo apt-get install ubuntustudio-font-meta
+
+
+# development basics
+#
+sudo apt-get install curl wget
+sudo apt-get install build-essential cmake
+sudo apt-get install git-core
+
+
+# neovim
+#
+sudo apt-get install software-properties-common python-software-properties
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+sudo apt-get python-dev python-pip
+pip install --user neovim
+
+
+# vim
+#
+# this can vary based on requirements, but I found vim-gnome is the "fat" version
+# and contains pretty much all the +flags, particularly +clipboard
+#
+# sudo apt-get install vim
+# sudo apt-get install vim-gtk
+sudo apt-get install vim-gnome
+
+
+# tmux
+#
+sudo apt-get install tmux
+
+
+# zsh
+#
+sudo apt-get install zsh
+
+
+# oh-my-zsh
+#
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+chsh -s $(which zsh) # you will most likely be prompted for a password
+
+# must logout/login to see zsh change
+
+# Change your terminal settings to encode in UTF-8 or you'll get some funky texts
+# and artifacting
+
+
+# fix jittery mouse, kind of works...
+#
+xinput --set-prop "Atmel maXTouch Touchpad" "Device Accel Constant Deceleration" 3
+# http://ubuntuforums.org/showthread.php?t=2152733
+# xinput list-prop <id>
+xinput --set-prop "Atmel maXTouch Touchpad" "Synaptics Noise Cancellation" 20 20
+
+
+# FIXME I might have missed some apt-gets, will need to pick those up next time around
+
+# after this it's all you
+#
+# TODO setup ssh key
+# TODO install dotfiles
+# TODO install Vim plugins
+# TODO install golang
