3222d5b6a47342a8d31533b8804c67714492acf4
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-04-29 14:37:45 -0400

    Create local.sh

---
+++ local.sh
@@ -0,0 +1,18 @@
+
+# after this it's all you
+#
+# TODO setup ssh key
+# TODO install dotfiles
+
+
+# terms
+# CD to prospect dir
+# tic screen-256color-italic.terminfo
+# tic xterm-256color-italic.terminfo
+
+
+# TODO install Vim plugins
+# TODO install golang
+
+# If required to build tig from source will need ncurses
+sudo apt-get install libncurses-dev
