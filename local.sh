02d7806e97387e2774203fbd25d2caae9f51c6e3
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-04-29 16:16:22 -0400

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,18 +1,35 @@
-
-# after this it's all you
+# Local setup starts here
 #
-# TODO setup ssh key
-# TODO install dotfiles
+# Assumed Git and Github are all setup at this point
 
+cd ~/
+
+# TODO dotfiles
+# git clone git@github.com:nowk/prospect.git
+# TODO rc links
 
 # terms
-# CD to prospect dir
-# tic screen-256color-italic.terminfo
-# tic xterm-256color-italic.terminfo
+#
+cd ~/.prospect/terms
+tic screen-256color-italic.terminfo
+tic xterm-256color-italic.terminfo
 
 
 # TODO install Vim plugins
-# TODO install golang
+
+
+# Go
+#
+mkdir ~/opt
+cd ~/opt
+git clone git@github.com:golang/go.git
+
+cd go
+git checkout go1.4.2
+
+cd src
+./all.bash
+
 
 # If required to build tig from source will need ncurses
 sudo apt-get install libncurses-dev
